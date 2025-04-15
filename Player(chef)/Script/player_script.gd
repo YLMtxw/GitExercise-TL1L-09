7caa69b9ebc816bb9fb66abfb8c5085117474db2
Kong Zhun Rui <kzrui2006@gmail.com>
2025-04-15 21:01:11 +0800

    remove animation code for debug and added run function

--- Player(chef)/Script/player_script.gd
+++ Player(chef)/Script/player_script.gd
@@ -1,35 +1,19 @@
 class_name player_character extends CharacterBody2D
 
-var movement_speed : int = 100
+const  movement_speed : int = 150
+const run_speed : int = 300
 #vector2 contain 2 num, x and y
 var direction : Vector2 = Vector2.ZERO
-var ori_direction : Vector2 = Vector2.DOWN
-var state : String = "idle"
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var sprite_2d: Sprite2D = $Sprite2D
-
-
-func _ready():
-	pass
 	
 func _process( delta ):
 	
 	direction.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
 	direction.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
-	velocity = direction * movement_speed
-	Animation()
-	
 
 func _physics_process( delta ):
+	var current_speed = movement_speed
+	if Input.is_key_pressed(KEY_SHIFT): 
+		current_speed = run_speed
+	velocity = direction * current_speed
 	move_and_slide()
-	
-func Direction() -> bool:
-	return true
-	
-func State() -> bool:
-	return true
-
-func Animation() -> void:
-	animation_player.play("idle_down")
-	pass
