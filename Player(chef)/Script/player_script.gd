2c5ebaea3d5adf98123723550803ab10ebc8b034
Kong Zhun Rui <kzrui2006@gmail.com>
2025-04-15 22:05:19 +0800

    sucessfully aded in the animation

--- Player(chef)/Script/player_script.gd
+++ Player(chef)/Script/player_script.gd
@@ -1,15 +1,30 @@
 class_name player_character extends CharacterBody2D
 
 const  movement_speed : int = 150
-const run_speed : int = 300
+const run_speed : int = 225
 #vector2 contain 2 num, x and y
 var direction : Vector2 = Vector2.ZERO
+@onready var animation = $AnimationPlayer
 
-	
 func _process( delta ):
 	
 	direction.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
 	direction.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
+	
+func UpdateDirection():
+	if direction == Vector2.ZERO:
+		animation.stop()
+	else:
+		var direction = "_down"
+		if velocity.y < 0: direction = "_up"
+		elif velocity.x > 0: 
+			$Sprite2D.flip_h = false
+			direction = "_leftright"
+		elif velocity.x < 0: 
+			$Sprite2D.flip_h = true
+			direction = "_leftright"
+		
+		animation.play("walk" + direction)
 
 func _physics_process( delta ):
 	var current_speed = movement_speed
@@ -17,3 +32,4 @@ func _physics_process( delta ):
 		current_speed = run_speed
 	velocity = direction * current_speed
 	move_and_slide()
+	UpdateDirection()
