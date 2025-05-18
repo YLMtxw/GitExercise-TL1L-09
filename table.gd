06bc5dfb053926fa6d5bb1e1216ed9b18091b8a9
ong zhong yik <ongzhongyik@email.com>
2025-05-18 17:09:56 +0800

    delete some rubbish and modify the create button

--- table.gd
+++ table.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var sprite_a = $photo1
 @onready var sprite_b = $photo2
+@onready var sprite_c = $photo3
 
 func _ready():
 	# 给所有按钮连接 signal
@@ -14,9 +15,18 @@ func _on_button_2_pressed() -> void:
 	# 显示 A，隐藏 B
 	sprite_a.visible = false
 	sprite_b.visible = true
+	sprite_c.visible = false
 
 
 func _on_button_1_pressed() -> void:
 		# 显示 A，隐藏 B
 	sprite_a.visible = true
 	sprite_b.visible = false
+	sprite_c.visible = false
+
+
+func _on_button_3_pressed() -> void:
+		# 显示 A，隐藏 B
+	sprite_a.visible = false
+	sprite_b.visible = false
+	sprite_c.visible = true
