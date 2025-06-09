26cac017377a45a36c25fa15480fdc40e7b8b4a1
Kong Zhun Rui <kzrui2006@gmail.com>
2025-06-09 17:08:15 +0800

    removed useless code

--- h_slider.gd
+++ h_slider.gd
@@ -1,7 +1,1 @@
 extends HSlider
-
-func _on_volume_slider_value_changed(value):
-	var bus_index = AudioServer.get_bus_index("Master")
-	AudioServer.set_bus_volume_db(bus_index, value)
-	AudioServer.set_bus_mute(bus_index, value <= -60)
-	Global.volume_db = value
