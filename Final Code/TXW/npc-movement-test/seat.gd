8c383fa44b7ea62d95e6b2a33055a7de9c6656fc
Kong Zhun Rui <kzrui2006@gmail.com>
2025-06-09 18:18:53 +0800

    removed file that xw accidentally push to my branch

--- Final Code/TXW/npc-movement-test/seat.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-var occupied: bool = false
-
-func _on_OccupyArea_body_entered(body):
-	if body is CharacterBody2D:
-		occupied = true
--- Final Code/TXW/ordering-logic/GitExercise-TL1L-09-KZR old ver/tutorial_button.gd
+++
@@ -1,5 +0,0 @@
-extends Button
-
-func _on_tutorial_button_pressed():
-	print("Tutorial button clicked!")
-	$PopupPanel.popup_centered()
