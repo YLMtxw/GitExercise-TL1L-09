086ec1d074a8d0151c71e8f5c884d01d79dff42a
YLMtxw <104744712+YLMtxw@users.noreply.github.com>
2025-06-11 01:15:06 +0800

    Delete Final Code/TXW directory

--- Final Code/TXW/ordering-logic/demand and random order logic - 6th june/parallax_background.gd
+++
@@ -1,6 +0,0 @@
-extends ParallaxBackground
-
-@export var scroll_speed: float = 30.0 #this is speed to control the backgorund
-
-func _process(delta):
-	scroll_offset.x += scroll_speed * delta
--- Final Code/TXW/ordering-logic/demand and random order logic - 6th june/start_button.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-func _on_StartButton_pressed():
-	$ClickSound.play()  # 播放按钮声音
-	get_tree().change_scene_to_file("res://PlayerInfo.tscn")
