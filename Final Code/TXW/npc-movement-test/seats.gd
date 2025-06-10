8894f7ba40a13bffe9de0721ed18e90fb4527aab
YLMtxw <104744712+YLMtxw@users.noreply.github.com>
2025-06-11 01:16:03 +0800

    Delete Final Code/TXW directory

--- Final Code/TXW/npc-movement-test/seats.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-var occupied := false
-
-func _on_OccupyArea_body_entered(body):
-	if body is CharacterBody2D:
-		occupied = true
--- Final Code/TXW/ordering-logic/demand and random order logic - 6th june/Inventory/iSlot.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-
-class_name iSlot
-
-@export var item : InventoryItem
-@export var itemNum : int
