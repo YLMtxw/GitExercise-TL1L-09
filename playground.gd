3c01735ec2ec3b687e9c7919d792cb89b8f4ce5d
Kong Zhun Rui <kzrui2006@gmail.com>
2025-06-14 15:01:08 +0800

    added save item price function

--- playground.gd
+++ playground.gd
@@ -2,10 +2,14 @@ extends Node2D
 
 @onready var bgm = $AudioStreamPlayer
 var last_saved_position := Vector2.ZERO
+@onready var aglio : InventoryItem = preload("res://Inventory/Item/aglio olio.tres")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Global.load_game(Global.current_store_name)
+	#test function
+	print("Aglio Price after load:", aglio.price)
+	
 	bgm.play()
 	
 
