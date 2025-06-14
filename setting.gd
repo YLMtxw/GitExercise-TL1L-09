d564b22c1e1208aa903861484feedddbf8b89f96
Kong Zhun Rui <kzrui2006@gmail.com>
2025-06-14 15:40:11 +0800

    save volume

--- setting.gd
+++ setting.gd
@@ -5,6 +5,7 @@ var scene = "res://playground.tscn"
 
 		
 func _ready():
+	Global.load_game("profile")
 	var bus_index = AudioServer.get_bus_index("Master")
 	AudioServer.set_bus_volume_db(bus_index, Global.volume_db)
 	$Setting/HSlider.value = Global.volume_db
@@ -32,6 +33,7 @@ func _on_volume_slider_value_changed(value):
 	AudioServer.set_bus_volume_db(bus_index, value)
 	AudioServer.set_bus_mute(bus_index, value <= -60)
 	Global.volume_db = value
+	Global.save_game()
 
 
 func _on_button_resume_pressed() -> void:
